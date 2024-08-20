304d46be0b9cb099431b2534ea024a0c6c8554f7
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-08-19 22:17:02 -0300

    toma

--- Programacao_scripts/Calculadora/index.js
+++ Programacao_scripts/Calculadora/index.js
@@ -1,12 +1,43 @@
-const { createApp } = Vue
+const  { createApp } = Vue;
 createApp({
     data() {
-        return{
-            teste: "2 + 2 = 5",
-            numero: 1
+        return {
+            display: '0',
+            numeroAtual: '',
+            numeroAnterior: '',
+            operador: null
         }
+
     },
     methods: {
+        lidarBotao(botao) {
+            switch (botao) {
+                case "x":
+                case '/':
+                case '+':
+                case '-':
+                    this.lidarOperador(botao)
+                    break
+                case ',':
+                    this.lidarDecimal()
+                    break
+                case '=':
+                    this.lidarIgual()
+                    break
+                case 'AC':
+                    this.lidarClear()
+                    break
+                default:
+                    this.lidarNumero(botao)
+            }
+        },
+        lidarNumero(bt) {
+            this.display += bt
+        },
+
+        lidarClear() {
+            this.display = '0'
+        },
 
     }
 }).mount("#app")
