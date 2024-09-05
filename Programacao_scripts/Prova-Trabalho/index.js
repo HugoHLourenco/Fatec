7380376ce0703f6be2189a556aec84579ef879e8
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-04 22:11:06 -0300

    att

--- Programacao_scripts/Prova-Trabalho/index.js
+++ Programacao_scripts/Prova-Trabalho/index.js
@@ -3,7 +3,7 @@ const  {createApp} = Vue;
 createApp({
     data() {
         return {
-            pokemon: {life: 100, name:'', xp: 0},
+            pokemon: {life: 100, name:'DHUASHDUS', xp: 0},
             oponent: {life: 100, name:'', xp: 0},
         }
     },
