d01a6ae17420262bd8a065c78f32cb6e2c128f13
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 15:36:55 -0300

    oponent sprite

--- Programacao_scripts/Prova-Trabalho/js/index.js
+++ Programacao_scripts/Prova-Trabalho/js/index.js
@@ -1,10 +1,10 @@
-const  {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            pokemon: {life: 100, name:'DHUASHDUS', xp: 0},
-            oponent: {life: 124, name:'Oponent Pokemon', maxLife: 331},
+            pokemon: { life: 100, name: 'DHUASHDUS', xp: 0 },
+            oponent: { life: 124, name: 'Oponent Pokemon', maxLife: 331 },
         }
     },
     methods: {
