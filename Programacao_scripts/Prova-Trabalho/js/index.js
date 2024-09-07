1950068a3b972c5efcf402eded93fd967ee8b252
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 18:46:25 -0300

    botoes

--- Programacao_scripts/Prova-Trabalho/js/index.js
+++ Programacao_scripts/Prova-Trabalho/js/index.js
@@ -6,13 +6,25 @@ createApp({
             pokemon: { life: 100, maxLife: 300, name: 'Rayquaza',},
             oponent: { life: 124, maxLife: 331, name: 'Giratina', },
             fighting: false,
-            bag: false,
+            bagStatus: false,
+            battling: false,
         }
     },
     methods: {
         fight(fighting){
             this.fighting = !this.fighting
-            
-        }
+        },
+
+        bag(bagStatus) {
+            this.bagStatus = !this.bagStatus
+        },
+
+        run(){
+            alert("Você fugiu...")
+        },
+
+        numTemMais(){
+            alert("Você não tem outros pokémons!")
+        },
     }
 }).mount('#app')
