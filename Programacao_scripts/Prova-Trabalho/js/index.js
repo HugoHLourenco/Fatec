75f9bb30acdf921b569a6793094282eba4816603
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 17:59:02 -0300

    muito dahora

--- Programacao_scripts/Prova-Trabalho/js/index.js
+++ Programacao_scripts/Prova-Trabalho/js/index.js
@@ -5,9 +5,14 @@ createApp({
         return {
             pokemon: { life: 100, maxLife: 300, name: 'Rayquaza',},
             oponent: { life: 124, maxLife: 331, name: 'Giratina', },
+            fighting: false,
+            bag: false,
         }
     },
     methods: {
-        
+        fight(fighting){
+            this.fighting = !this.fighting
+            
+        }
     }
 }).mount('#app')
