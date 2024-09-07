d59e3bc92b1bbb14f9a9c2dc11082a688441053d
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 17:17:40 -0300

    fazendo o menu de acoes

--- Programacao_scripts/Prova-Trabalho/js/index.js
+++ Programacao_scripts/Prova-Trabalho/js/index.js
@@ -3,8 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            pokemon: { life: 100, maxLife: 300, name: 'seus pokemon',},
-            oponent: { life: 124, maxLife: 331, name: 'Mewtwo', },
+            pokemon: { life: 100, maxLife: 300, name: 'Rayquaza',},
+            oponent: { life: 124, maxLife: 331, name: 'Giratina', },
         }
     },
     methods: {
