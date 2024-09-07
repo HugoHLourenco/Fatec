29b6d14f435121dcceb9ed2e694c82f17ef55a48
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 16:49:56 -0300

    mewtwo na batalha

--- Programacao_scripts/Prova-Trabalho/js/index.js
+++ Programacao_scripts/Prova-Trabalho/js/index.js
@@ -3,11 +3,11 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            pokemon: { life: 100, name: 'DHUASHDUS', xp: 0 },
-            oponent: { life: 124, name: 'Oponent Pokemon', maxLife: 331 },
+            pokemon: { life: 100, maxLife: 300, name: 'seus pokemon',},
+            oponent: { life: 124, maxLife: 331, name: 'Mewtwo', },
         }
     },
     methods: {
-
+        
     }
 }).mount('#app')
