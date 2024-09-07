ff24924487de911339c621741285a581b94b068c
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 14:53:09 -0300

    CSS att

--- Programacao_scripts/Prova-Trabalho/index.js
+++ Programacao_scripts/Prova-Trabalho/index.js
@@ -4,7 +4,7 @@ createApp({
     data() {
         return {
             pokemon: {life: 100, name:'DHUASHDUS', xp: 0},
-            oponent: {life: 100, name:'', xp: 0},
+            oponent: {life: 243, name:'teste 313dsadas', maxLife: 300},
         }
     },
     methods: {
