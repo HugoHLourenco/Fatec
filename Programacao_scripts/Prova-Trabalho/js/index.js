de41d139b5d4d273fa044b587f2d41481eac4e4a
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-07 22:14:06 -0300

    mecanica de batalha

--- Programacao_scripts/Prova-Trabalho/js/index.js
+++ Programacao_scripts/Prova-Trabalho/js/index.js
@@ -3,31 +3,38 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            pokemon: { 
-                life: 351, 
-                maxLife: 351, 
-                name: 'Rayquaza', 
-                attack: 336, 
-                defense: 216, 
-                speed: 226, },
+            pokemon: {
+                life: 351,
+                maxLife: 351,
+                name: 'Rayquaza',
+                attack: 336,
+                defense: 216,
+                speed: 226,
+                move: "",
+            },
 
-            oponent: { life: 441, 
-                maxLife: 441, 
-                name: 'Giratina', 
-                attack: 276, 
-                defense: 236, 
-                speed: 216,},
+            oponent: {
+                life: 441,
+                maxLife: 441,
+                name: 'Giratina',
+                attack: 276,
+                defense: 236,
+                speed: 216,
+                move: "",
+            },
 
-                
+
             fighting: false,
             bagStatus: false,
             battling: false,
             potion: true,
+            battle: 0,
+            mensage: "",
         }
     },
     methods: {
         // Botões de ações ----------------------------------------------------------------
-        fight(fighting){
+        fight(fighting) {
             this.fighting = !this.fighting
         },
 
@@ -35,21 +42,28 @@ createApp({
             this.bagStatus = !this.bagStatus
         },
 
-        run(){
+        run() {
             alert("Você fugiu...")
             window.location.reload();
         },
 
-        numTemMais(){
+        numTemMais() {
             alert("Você não tem outros pokémons!")
         },
 
-        usePotion(){
+        usePotion() {
             this.bagStatus = false
             this.potion = false
             this.pokemon.life = 351
         },
 
+        next() {
+            this.battle += 1
+            this.attackShadowBall()
+            if (this.battle >= 3) {
+                this.battle = 0
+            }
+        },
 
 
         // Pokemon Ataques -------------------------------------------------------------
@@ -58,6 +72,8 @@ createApp({
             if (this.oponent.life <= 0) {
                 this.oponent.life = 0
             }
+            this.battle += 1
+            this.pokemon.move = "Thunder"
         },
 
         attackTwister() {
@@ -65,6 +81,8 @@ createApp({
             if (this.oponent.life <= 0) {
                 this.oponent.life = 0
             }
+            this.battle += 1
+            this.pokemon.move = "Twister"
         },
 
         attackCrunch() {
@@ -72,15 +90,29 @@ createApp({
             if (this.oponent.life <= 0) {
                 this.oponent.life = 0
             }
+            this.battle += 1
+            this.pokemon.move = "Crunch"
         },
 
         attackBulkUp() {
             this.pokemon.attack *= 1.15
+            this.battle += 1
+            this.pokemon.move = "Bulk Up"
         },
 
         // Oponent ataques --------------------------------------------------------------
-        attackRest() {
 
+        ia(battle) {
+            if (this.battle === 3) {
+                if (this.oponent.life <= this.oponent.maxLife * 0.9) {
+                    this.attackRest()
+                }
+            }
+        },
+
+        attackRest() {
+            this.oponent.life = 441
+            this.oponent.move = "Rest"
         },
 
         attacksleepTalk() {
@@ -88,11 +120,17 @@ createApp({
         },
 
         attackShadowBall() {
-
+            this.pokemon.life -= Math.floor(((this.oponent.attack / this.pokemon.defense) * 110))
+            if (this.pokemon.life <= 0) {
+                this.pokemon.life = 0
+            }
+            this.battle += 1
+            this.oponent.move = "Shadow Ball"
         },
 
         attackDragonClaw() {
 
         },
+
     }
 }).mount('#app')
