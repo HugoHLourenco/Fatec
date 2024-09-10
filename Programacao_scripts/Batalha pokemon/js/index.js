93fbe260509b1360932d3caa3b5041e189c93ad2
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-09 23:50:33 -0300

    Update index.js

--- Programacao_scripts/Batalha pokemon/js/index.js
+++ Programacao_scripts/Batalha pokemon/js/index.js
@@ -30,7 +30,7 @@ createApp({
             fighting: false, // Verficador se "fight" está ativo
             bagStatus: false, // Verficador se "bag" está ativa
             battling: false, // Verficador se estão batalhando
-            pokemonBar: false,
+            pokemonBar: false, // Verificador se a Pokemons Bar está ativa
             potion: true, // Contador da potion
             battle: 0, // Contador de turno de batalha
             runn: false, // Verificador de fuga
