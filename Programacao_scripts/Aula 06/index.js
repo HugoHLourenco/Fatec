7f132bd282d024b267b03caf6059ac8fdd2e1f36
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-30 22:02:33 -0300

    Update index.js

--- Programacao_scripts/Aula 06/index.js
+++ Programacao_scripts/Aula 06/index.js
@@ -10,12 +10,31 @@ createApp({
     },
 
     methods: {
+        
         async callAPI(){
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${this.nextPage - 1}&limit=${385}`)
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${this.nextPage - 1}&limit=${385}`)
+                
+                const data = await response.json()
+                
+                const pokemonDetailsPromisses = data.results.map(async pokemon => this.fetchPokemonData(pokemon.url)) 
 
-            const data = await response.json()
+                console.log(pokemonDetailsPromisses)
 
-            const pokemonDetailsPromisses = data.results.map(async)
+                const pokemonDetails = await Promise.all(pokemonDetailsPromisses)
+            } catch (error) {
+                console.error(error)
+            }
+        },
+
+        async fetchPokemonData(url) {
+            try {
+                const response = await fetch(url)
+                const data = await response.json()
+                return data
+            } catch (e) {
+                console.error(e)
+            }
         }
     }
 }).mount('#app');
