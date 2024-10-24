85b04472ae2392e93c158c98003cdd90d0474a79
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-10-23 21:38:24 -0300

    dsad

---
+++ Programacao_scripts/Aula_08/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineComponent } from "vue";
+import Aula08 from "./components/Aula 08/Aula08.vue";
+export default defineComponent (
+  {
+    name: "App",
+    components: {
+      Aula08
+    }
+  },
+)
+</script>
+
+<template>
+  <aula08></aula08>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Programacao_scripts/Aula_08/src/components/Aula 08/Aula08.vue
@@ -0,0 +1,24 @@
+<script>
+import Pessoa from "./Pessoa.vue";
+export default {
+  name: "Aula 08",
+  components: {
+    Pessoa,
+  },
+  data() {
+    return {
+      idade: 10
+    },
+  }
+}
+</script>
+
+<template>
+  <Pessoa idade="10" @update-idade="idade++"></Pessoa>
+</template>
+
+<style scoped>
+  p{
+    color: #00BFFF;
+  }
+</style>
